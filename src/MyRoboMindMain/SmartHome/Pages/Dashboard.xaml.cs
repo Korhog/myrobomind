a7993a681dfa8e8b6034944354630ebcad0e41f7
Eugeny Klokov <grafinfector@hotmail.com>
2018-05-31 17:01:09 +0300

    [smart] убрал лишнее

--- src/MyRoboMindMain/SmartHome/Pages/Dashboard.xaml.cs
+++ src/MyRoboMindMain/SmartHome/Pages/Dashboard.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,6 +29,18 @@ namespace SmartHome.Pages
         {
             this.InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Enabled;
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            var color = Colors.Transparent;
+            titleBar.ButtonBackgroundColor = color;
+            titleBar.ButtonInactiveBackgroundColor = color;
+            titleBar.BackgroundColor = color;
+            titleBar.ForegroundColor = Colors.White;
+            titleBar.ButtonInactiveBackgroundColor = color;
         }
 
         private void OnPageChange(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
